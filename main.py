47e616077a48e8149a28ecd1c7042fc48d1dcdd1
Uberi <azhang9@gmail.com>
2014-05-31 16:45:41 -0400

    Update

--- main.py
+++ main.py
@@ -1,33 +1,20 @@
-import os, sys
-import random
-import string
-import hashlib
-
+import os
 import cherrypy
 
-BASE_PATH = os.path.join(os.path.abspath(os.getcwd())
+BASE_PATH = os.path.abspath(os.getcwd())
 
-# set up Mako templating
-import lib.mako_templating.makoplugin as makoplugin
-import lib.mako_templating.makotool as makotool
-makoplugin.MakoTemplatePlugin(cherrypy.engine, base_dir=BASE_PATH).subscribe() # register the Mako plugin
-cherrypy.tools.template = makotool.MakoTool() # register the Mako tool
+import lib.login
+import lib.mako_templating
+import lib.user_page
 
 class Qodex(object):
+    def __init__(self):
+        self.login = lib.login.Login()
+
     @cherrypy.expose
     @cherrypy.tools.template(template="template/index.mako")
     def index(self):
         cherrypy.response.status = 200
-
-    @cherrypy.expose
-    def generate(self, length=8):
-        some_string = "".join(random.sample(string.hexdigits, int(length)))
-        cherrypy.session["mystring"] = some_string
-        return some_string
-
-    @cherrypy.expose
-    def display(self):
-        return cherrypy.session["mystring"]
 
 if __name__ == "__main__":
     configuration = {
@@ -41,7 +28,7 @@ if __name__ == "__main__":
         },
         "/favicon.ico": {
           "tools.staticfile.on": True,
-          "tools.staticfile.filename": BASE_PATH, "./favicon.ico"),
+          "tools.staticfile.filename": os.path.join(BASE_PATH, "./favicon.ico"),
         },
     }
     cherrypy.config.update("config.cfg")
