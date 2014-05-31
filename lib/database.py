0d4aaab753ff3f908b8428fd4186ae702b21fb80
Elvin Yung <elvinyung1@gmail.com>
2014-05-31 17:24:09 -0400

    fixed table type

--- lib/database.py
+++ lib/database.py
@@ -5,13 +5,23 @@ from sqlalchemy import create_engine
  
 Base = declarative_base()
 
+# many-by-many relationship tables
+user_group = Table('user_groups', Base.metadata,
+    Column('user_id', Integer, ForeignKey('user.id')),
+    Column('group_id', Integer, ForeignKey('group.id')))
+
+group_books = Table('group_books', Base.metadata,
+    Column('group_id', Integer, ForeignKey('group.id')),
+    Column('book_id', Integer, ForeignKey('book.id')))
+
+
 class User(Base):
     __tablename__ = "user"
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-    password_hash = Column(String(64), nullable=False)
+    password = Column(String(64), nullable=False)
     email_address = Column(String(250), nullable=False)
-    groups = relationship('Group', user_group, backref='user')
+    groups = relationship('Group', secondary=user_group, backref='user')
 
     # query_by_id: returns the User instance with the given id.
     @staticmethod
@@ -22,28 +32,20 @@ class User(Base):
     def query_by_email_address(session, email_address):
         session.query(User).filter_by(email_address=email_address).first()
 
+    @staticmethod
+    def authenticate(self, password):
+        return (password == self.password)
+
 class Group(Base):
     __tablename__ = "group"
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-    books = relationship('Book', group_books, backref='group')
+    books = relationship('Book', secondary=group_books, backref='group')
 
 class Book(Base):
     __tablename__ = "book"
     id = Column(Integer, primary_key=True)
     title = Column(String(250), nullable=False)
-
-
-
-# many-by-many relationship tables
-user_group = Table('user_groups', Base.metadata,
-    Column('user_id', Integer, ForeignKey('user.id')),
-    Column('group_id', Integer, ForeignKey('group.id')))
-
-group_books = Table('group_books', Base.metadata,
-    Column('group_id', Integer, ForeignKey('user.id')),
-    Column('book_id', Integer, ForeignKey('book.id')))
-
 
 '''
 class Address(Base):
