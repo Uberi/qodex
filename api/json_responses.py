28af5a4750c6da3dfa3f7078275c7ac705b1965e
Elvin Yung <elvinyung1@gmail.com>
2014-06-01 03:38:26 -0400

    added things

--- api/json_responses.py
+++ api/json_responses.py
@@ -8,7 +8,9 @@ cherrypy.tools.user = lib.user_page.UserTool()
 from lib.models.models import User, Group, Book, session_scope
 
 def dict_user(user):
-	return {('user%s' % user.id):{'id':user.id, 'email':user.email, 'latitude':user.latitude, 'longitude':user.longitude, 'bio':user.bio}}
+	# TODO: add list of groups the user is in to the json
+	with session_scope() as s:
+		return {('user%s' % user.id):{'id':user.id, 'email':user.email, 'latitude':user.latitude, 'longitude':user.longitude, 'bio':user.bio, 'groups':user.groups}}
 
 def jsonify_user(user):
 	return json.dumps(dict_user(user))
