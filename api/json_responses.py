6d8c243ad6112edccb6aafddf69e192931702bb1
Elvin Yung <elvinyung1@gmail.com>
2014-06-01 01:32:26 -0400

    jsonify dictionaryify

--- api/json_responses.py
+++ api/json_responses.py
@@ -11,7 +11,10 @@ def dict_user(user):
 	return {('user%s' % user.id):{'id':user.id, 'email':user.email, 'latitude':user.latitude, 'longitude':user.longitude}}
 
 def jsonify_user(user):
-	return json.dumps({('user%s' % user.id):{'id':user.id, 'email':user.email, 'latitude':user.latitude, 'longitude':user.longitude}})
+	return json.dumps(dict_user(user))
+
+def dict_book(book):
+	return {('book%s' % book.id):{'id':book.id, 'title':book.title, 'isbn':book.isbn, 'author':book.author}}
 
 def jsonify_book(book):
-	return json.dumps({('book%s' % book.id):{'id':book.id, 'title':book.title, 'isbn':book.isbn, 'author':book.author}})
+	return json.dumps(dict_book(book))
