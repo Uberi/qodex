8d887a614460b376c726466050a5478a73542859
Uberi <azhang9@gmail.com>
2014-05-31 23:36:08 -0400

    Update.

--- api/__init__.py
+++ api/__init__.py
@@ -1,5 +1,11 @@
 import json
 from lib.models import models
+import cherrypy
+
+import lib.user_page
+cherrypy.tools.user = lib.user_page.UserTool()
+
+from lib.models.models import User, Group, Book, session_scope
 
 # RESTful API
 class UserAPI(object):
@@ -13,12 +19,15 @@ class UserAPI(object):
         user = User(name, password, email, latitude, longitude)
         with session_scope() as s:
             s.add(user)
+            return "OK"
+        return "FAIL"
 
     @cherrypy.expose
     @cherrypy.tools.user()
     def logout(self):
         user = cherrypy.request.user
         del cherrypy.session["user_id"]
+        return "OK"
 
     @cherrypy.expose
     @cherrypy.tools.user()
@@ -26,3 +35,5 @@ class UserAPI(object):
         user = cherrypy.request.user
         with session_scope() as s:
             s.delete(user)
+            return "OK"
+        return "FAIL"
--- main.py
+++ main.py
@@ -2,49 +2,19 @@ import os
 import json
 import cherrypy
 
-BASE_PATH = os.path.abspath(os.getcwd())
+BASE_PATH = os.path.dirname(os.path.realpath(__file__))
 
 import lib.login
-import lib.mako_templating
-import lib.user_page
-
-cherrypy.tools.user = lib.user_page.UserTool()
-
-from lib.models.models import User, Group, Book, session_scope
-
-class Users(object):
-    @cherrypy.expose
-    def index(self, filter = ""):
-        with session_scope() as s:
-            return json.dumps(User.list(s, filter))
-    
-    @cherrypy.expose
-    def create(self, name, password, email, latitude, longitude):
-        user = User(name, password, email, latitude, longitude)
-        with session_scope() as s:
-            s.add(user)
-
-    @cherrypy.expose
-    @cherrypy.tools.user()
-    def logout(self):
-        user = cherrypy.request.user
-        del cherrypy.session["user_id"]
-
-    @cherrypy.expose
-    @cherrypy.tools.user()
-    def delete(self):
-        user = cherrypy.request.user
-        with session_scope() as s:
-            s.delete(user)
+import api
 
 class Qodex(object):
     def __init__(self):
         self.login = lib.login.Login()
-        self.users = Users()
+        self.users = api.UserAPI()
 
     @cherrypy.expose
     def index(self):
-        return cherrypy.lib.static.serve_file(os.path.join(BASE_PATH, "./static/index.html"))
+        return cherrypy.lib.static.serve_file(os.path.join(BASE_PATH, "static/index.html"))
     
     @cherrypy.expose
     def groups(self, filter = ""):
@@ -55,15 +25,9 @@ if __name__ == "__main__":
     configuration = {
         "/": {
             "tools.sessions.on": True,
-            "tools.staticdir.root": BASE_PATH,
-        },
-        "/static": {
             "tools.staticdir.on": True,
             "tools.staticdir.dir": "./static",
-        },
-        "/favicon.ico": {
-          "tools.staticfile.on": True,
-          "tools.staticfile.filename": os.path.join(BASE_PATH, "./favicon.ico"),
+            "tools.staticdir.root": BASE_PATH,
         },
     }
     cherrypy.config.update("config.cfg")
